3c15be7ca1716adabdfdc584040d903981ab0613
smb2255 <smb2255@columbia.edu>
2018-01-29 15:15:23 -0500

    sign in works now

--- assets/scripts/events.js
+++ assets/scripts/events.js
@@ -15,6 +15,7 @@ const onSignUp = function (event) {
 const onSignIn = function (event) {
   event.preventDefault()
   const data = getFormFields(event.target)
+  console.log('onSignIn reached')
   api.signIn(data)
     .then(ui.signInSuccess)
     .catch(ui.signInFailure)
@@ -32,7 +33,7 @@ const onChangePass = function (event) {
 // }
 const addHandlers = function () {
   $('#sign-up-form').on('submit', onSignUp)
-  $('#user-messages').on('submit', onSignIn)
+  $('#sign-in-form').on('submit', onSignIn)
   $('#user-messages').on('submit', onChangePass)
 }
 module.exports = {
