eba066df47aba50e498c59ab0a9cbb9469a3ea28
smb2255 <smb2255@columbia.edu>
2018-02-08 17:41:10 -0500

    the sign in/up works now

---
+++ assets/scripts/userEvents.js
@@ -0,0 +1,95 @@
+'use strict'
+const getFormFields = require('../../lib/get-form-fields')
+const api = require('./api.js')
+const ui = require('./ui.js')
+// const store = require('./store.js')
+
+const introSignIn = function (event) {
+  event.preventDefault()
+  $('#intro').hide()
+  $('#sign-up-style').hide()
+  $('#sign-in-style').show()
+  $('#navigation').show()
+  $('#sign-space').show()
+}
+
+const introSignUp = function (event) {
+  event.preventDefault()
+  $('#intro').hide()
+  $('#sign-in-style').hide()
+  $('#sign-up-style').show()
+  $('#navigation').show()
+  $('#sign-space').show()
+}
+
+const onSignUp = function (event) {
+  event.preventDefault()
+  const data = getFormFields(event.target)
+  console.log('onSignUp reached')
+  api.signUp(data)
+    .then(ui.signUpSuccess)
+    .catch(ui.signUpFailure)
+  $('#sign-up-form').find('input[type=text], textarea').val('')
+  $('#sign-up-form').find('input[type=password], textarea').val('')
+}
+
+// Add click events for the sign up section form; prevents refresh.
+const addSignUpHandlers = function () {
+  $('#sign-up-form').on('submit', onSignUp)
+}
+
+const addSignInHandlers = function () {
+  $('#sign-in-form').on('submit', onSignIn)
+}
+
+// Add click events for the intro section buttions.
+const addIntroHandlers = function () {
+  $('#sign-in-button-intro').on('click', introSignIn)
+  $('#sign-up-button-intro').on('click', introSignUp)
+}
+const onSignIn = function (event) {
+  event.preventDefault()
+  const data = getFormFields(event.target)
+  console.log('onSignIn reached')
+  api.signIn(data)
+    .then(ui.signInSuccess)
+    .catch(ui.signInFailure)
+}
+const onChangePass = function (event) {
+  const data = getFormFields(event.target)
+  console.log('onChangePass reached')
+  event.preventDefault()
+  api.changePass(data)
+    .then(ui.changePassSuccess)
+    .catch(ui.changePassFailure)
+}
+const onSignOut = function (event) {
+  event.preventDefault()
+  console.log('onSignOut reached')
+  api.signOut()
+    .then(ui.signOutSuccess)
+    .catch(ui.signOutFailure)
+}
+
+// const addHandlers = function () {
+//   $('#sign-in-button').on('click', introSignIn)
+//   $('#sign-up-button').on('click', introSignUp)
+//   $('#sign-up-link').on('click', introSignUp)
+//   $('#sign-up-form').on('submit', onSignUp)
+//   $('#sign-in-form').on('submit', onSignIn)
+//   $('#change-password-form').on('submit', onChangePass)
+//   $('#sign-out-form').on('submit', onSignOut)
+// }
+
+module.exports = {
+  introSignIn,
+  introSignUp,
+  onSignUp,
+  onSignIn,
+  onChangePass,
+  onSignOut,
+  // addHandlers,
+  addSignUpHandlers,
+  addSignInHandlers,
+  addIntroHandlers
+}
---
+++ assets/scripts/userUi.js
@@ -0,0 +1,71 @@
+'use strict'
+const store = require('./store')
+
+const signUpSuccess = function (data) {
+  store.user = data.user
+  console.log('Sign up is successful')
+  $('#user-messages').html(`<p> Sign up success! </p>`)
+  $('#change-password').hide()
+}
+
+const signUpFailure = function () {
+  $('#user-messages').html(`<p>Sign-up failed!</p>`)
+}
+
+const signInSuccess = function (response) {
+  store.user = response.user
+  $('#greeting-space').show()
+  $('#sign-space').hide()
+  $('#sign-in').hide()
+  const userGreet = store.user.email.split('@')
+  if (userGreet[0].length < 16) {
+    $('.user-greeting').html(userGreet[0]).css('text-transform', 'uppercase')
+  } else {
+    $('.user-greeting').html('Friend').css('text-transform', 'uppercase')
+  }
+
+  // $('#user-messages').html(`<p>You have signed in!</p>`)
+  // $('#sign-up-style').hide()
+  // $('#sign-in').hide()
+  // $('#change-password').show()
+  // $('#new-run').show()
+  // $('#sign-out').show()
+  // $('#show-all-runs').show()
+  // $('#delete-run').show()
+}
+
+const signInFailure = function () {
+  $('#user-messages').html(`<p>Sign-in failed!</p>`)
+}
+
+const changePassSuccess = function () {
+  $('#user-messages').html('<p> You have changed your password </p>')
+  $('#change-password').hide()
+}
+
+const changePassFailure = function () {
+  $('#user-messages').html('<p> You have not changed your password </p>')
+}
+const signOutSuccess = function () {
+  store.user = null
+  $('#user-messages').html(`<p>Sign-out success!</p>`)
+  $('#change-password').hide()
+  $('#sign-out').hide()
+  $('#new-run').hide()
+  $('#sign-in').show()
+  $('#sign-up').show()
+}
+const signOutFailure = function () {
+  $('#user-messages').html(`<p>Sign-out failed!</p>`)
+}
+
+module.exports = {
+  signUpSuccess,
+  signUpFailure,
+  signInSuccess,
+  signInFailure,
+  changePassSuccess,
+  changePassFailure,
+  signOutSuccess,
+  signOutFailure
+}
